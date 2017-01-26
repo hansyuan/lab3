d7fe084b5914c463ad4970f241ed98a23d090be1
hanzzeh@gmail.com <hanzzeh@gmail.com>
2017-01-26 04:00:03 -0800

    finish and did stretch

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -6,14 +6,27 @@
 exports.view = function(req, res){
 
 
-    var name1 = req.params.userName
+    var name1 = req.params.date;
+    var date = req.params.date;
+    //console.log("THIS IS MY DATE:\n")
+    //console.log(date);
 
     if(name1 === undefined) {
         name1 = 'World';
     }
 
-    console.log("name is " + name1)
+  //  console.log("name is " + name1)
+  
     res.render('index', {
-  	 'name': name1,
+  	 'name': name1, date,
     });
+    
+
+
+    //var mydate = req.param.date;
+/*
+    res.render('index', {
+        'date': mydate,
+            });
+    */
 };
