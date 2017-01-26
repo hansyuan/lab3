165d5f8637f77bdbdb5b2b699fd63d295edad8ca
hanzzeh@gmail.com <hanzzeh@gmail.com>
2017-01-26 03:20:00 -0800

    nodejs

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -4,7 +4,16 @@
  */
 
 exports.view = function(req, res){
-  res.render('index', {
-  	'name': '',
-  });
+
+
+    var name1 = req.params.userName
+
+    if(name1 === undefined) {
+        name1 = 'World';
+    }
+
+    console.log("name is " + name1)
+    res.render('index', {
+  	 'name': name1,
+    });
 };
